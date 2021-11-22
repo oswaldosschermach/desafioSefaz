ab967d25c4bf1a2169de4bea762e9c582b0d590c
Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
2021-11-22 12:17:52 -0300

    Pequenos ajustes no código.

--- src/main/java/aulas/web/adivinhe/jpa/entity/Jogador.java
+++ src/main/java/aulas/web/adivinhe/jpa/entity/Jogador.java
@@ -8,12 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -29,7 +27,6 @@ import javax.validation.constraints.Size;
  *           nome varchar(100) not null,
  *           email varchar(200) not null,
  *           data_nasc date not null,
- *           senha varchar(200) not null,
  *           primary key(codigo)
  *      );
  */
@@ -40,7 +37,7 @@ public class Jogador implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer codigo;
 
     @NotNull
